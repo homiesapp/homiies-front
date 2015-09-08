f83dd8a8f9826c344f5c47a10681f6bf17acecca
torbas <t.orbasido@gmail.com>
2015-09-08 16:06:55 -0700

    add circle to center of home

--- app/components/home.js
+++ app/components/home.js
@@ -25,9 +25,9 @@ module.exports = React.createClass({
 			<div className="home row">
 				<div className="side-menu close"><SideMenu /></div>
 				<div className="col-sm-9 main-center-div">
-					<div className="col-md-5"></div>
-					<div className="col-md-2"><h1>TEXT</h1></div>
-					<div className="col-md-5"></div>
+					<div className="col-md-4"></div>
+					<div className="col-md-8 event-circle"></div>
+					<div className="col-md-4"></div>
 				</div>
         <div className="col-sm-3 friends-column">
 					<Friends homies={friends}/>
