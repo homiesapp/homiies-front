e154894b7a31259208dfbd2a7eef25acc0143d55
evertr <evertrotshuizen@gmail.com>
2015-09-08 18:19:44 -0700

    added profile/event elements to sidemenu

--- app/components/currentevent.js
+++ app/components/currentevent.js
@@ -1,5 +1,4 @@
 var React = require('react');
-var ButtonAction = require('./buttonaction');
 
 
 module.exports = React.createClass({
@@ -9,18 +8,9 @@ module.exports = React.createClass({
 	render: function() {
 			var currentEvent = this.props.currentEvent;
 			return (
-
 				<div className="current-event">
 					<div>
 						<h4>{currentEvent.currentEvent}</h4>
-					</div>
-					<div>
-						<div className="col-md-6">
-							<ButtonAction className="event-accept-button btn btn-success" onClick={this.handleClick} text={'Accept'} />
-						</div>
-						<div className="col-md-6">
-							<ButtonAction className="event-decline-button btn btn-danger" onClick={this.handleClick} text={'Decline'} />
-						</div>
 					</div>
 				</div>
 				);
--- app/components/profile.js
+++ app/components/profile.js
@@ -1,11 +1,15 @@
 var React = require('react');
-
+var Picture = require('./picture')
 
 module.exports = React.createClass({
   render: function() {
 
     return (
-      <div className="profile"></div>
+      <div className="profile">
+        <div className="col-md-12">
+          <Picture imageClass="image-profile" imageSource="https://photos-1.dropbox.com/t/2/AAC4R39xlBoeVVh1eVX-hjR3PX6OKJeCojF_u_3FD6lngg/12/389166994/jpeg/32x32/1/1441756800/0/2/Jill%20Greenwood.jpg/CJLvyLkBIAEgAiADIAcoAigDKAc/al8WS61UJTVoTDEW91GrsFseioGo0THIvVjg5NHZfZQ?size=1280x960&size_mode=2" />
+        </div>
+      </div>
     )
   }
 });
