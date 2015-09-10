0c4b56e5bf3d4aeb71f2eac32ce782c784bdff17
torbas <t.orbasido@gmail.com>
2015-09-10 15:55:31 -0700

    apply gmaps through map component

--- app/components/map.js
+++ app/components/map.js
@@ -1,14 +1,32 @@
 var React = require('react');
+var Gmap = require('../../node_modules/gmaps');
 
 module.exports = React.createClass({
-  clickHandler: function(){
-    this.props.clickHandler(newEvent);
+  componentDidMount(){
+
+    // Only componentDidMount is called when the component is first added.
+    // This makes sure that our map initialization code is run the first time.
+
+    this.componentDidUpdate();
   },
-  render: function() {
+
+  componentDidUpdate(){
+    
+    var map = new GMaps({
+      div: '#map',
+      lat: -12.043333,
+      lng: -77.028333,
+      width: '200px',
+      height: '200px',
+    });
+  },
+
+  render(){
+
     return (
-      <div>
-        This is a map
+      <div className="map-holder" id="test">
+        <div id="map"></div>
       </div>
-      );
+    );
   }
 });
