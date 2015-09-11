fd52bee290f884b31be5cbb528b6ae6cedfda759
torbas <t.orbasido@gmail.com>
2015-09-11 14:52:13 -0700

    bootstrap modal appearing but still need to set events

--- app/components/home.js
+++ app/components/home.js
@@ -24,17 +24,19 @@ module.exports = React.createClass({
 				<div className="col-sm-2 side-menu">
 					<SideMenu />
 				</div>
-				<div className="col-sm-8 main-center-div hide">
+				<div className="col-sm-8 main-center-div">
 					<div className="event-create">
-						<div className="col-sm-12 event-circle"><div className="event-circle-text">Create Event</div></div>
+						<div className="col-sm-12 event-circle">
+							<div className="event-circle-text">Create Event</div>
+							<Modal isOpen={true}/>
+						</div>
 					</div>
 				</div>
-				<div className="col-sm-8 main-center-div">
+				<div className="col-sm-8 main-center-div hide">
 					<div className="col-md-12 event-info">
 						<div className="row">
 							<div className="col-md-6 bred quadrant">
 								<h1>SWIPE</h1>
-								<Modal isOpen={true}/>
 							</div>
 							<div className="col-md-6 bblue quadrant"><h1>Votes</h1></div>
 						</div>
