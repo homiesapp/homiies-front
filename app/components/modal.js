eb5475e63cf25a1df866e401b1780783c2241254
torbas <t.orbasido@gmail.com>
2015-09-11 15:52:41 -0700

    modal can close by clicking X

--- app/components/modal.js
+++ app/components/modal.js
@@ -25,7 +25,7 @@ module.exports = React.createClass({
 
           <div className="modal-content">
             <div className="modal-header">
-              <button type="button" className="close" data-dismiss="modal">&times;</button>
+              <ButtonAction className="close" clickHandler={this.handleCloseModal} text="&times;" />
               <h4 className="modal-title">Modal Header</h4>
             </div>
             <div className="modal-body">
