688956097f808e95a37b9f08a0f2dd8412965f71
Andrea Cabral <andrea.cabralb@gmail.com>
2015-09-13 16:37:40 -0700

    resolved bug couldnt load button_action.js file in signup.js

--- app/components/authentication.js
+++ app/components/authentication.js
@@ -5,19 +5,13 @@ import request from "superagent";
 
 module.exports = React.createClass({
 	onLoginHandler: function () {
-		var host = 'http://localhost:3000';  //'https://boiling-beyond-5952.herokuapp.com';
-
-		request
-			.get(host + '/gettofuckingfb')
-			.end(function(err, res) {
-					console.log('connection to fb', err);
-        });
+		console.log('bring me to fb!');
 	},
 	render: function() {
 		return (
 			<div>
 				<Login onLogin={this.onLoginHandler} />
 			</div>
-			);
+		);
 	}
 });
