2650833dfb744614898b75132f6d662e1ea35dac
torbas <t.orbasido@gmail.com>
2015-09-14 19:03:04 -0700

    add click to change class for inv

--- app/components/friend.js
+++ app/components/friend.js
@@ -3,12 +3,31 @@ var FriendInvite = require('./friend_invite');
 var Picture = require('./picture');
 
 module.exports = React.createClass({
+  getInitialState: function(){
+    return {
+      invited: false,
+    }
+  },
+
+  setClass: function() {
+    var invBool = !this.state.invited;
+
+    this.setState({
+      invited: invBool,
+    });
+  },
+
   render: function() {
 
     var user = this.props.homiie;
+    var friendClass = "friend";
+
+    if(this.state.invited){
+      friendClass = friendClass + " clicked"
+    }
 
     return (
-      <div className="friend">
+      <div className={friendClass} onClick={this.setClass}>
         <div className="row">
           <div className="col-md-4">
             <Picture imageClass="friend-picture" imageSource="http://blog.texasbar.com/files/2011/12/housto-bankruptcy-attorney-adam-schachter1.jpg" />
